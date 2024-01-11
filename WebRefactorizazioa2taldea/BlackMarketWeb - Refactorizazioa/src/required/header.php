7bed94f2de2744d52e712872dad0010193d2a6e1
AsierSaizar <asier.saizar.diaz@gmail.com>
2024-01-11 21:47:09 +0100

    side bar jarrita

---
+++ WebRefactorizazioa2taldea/BlackMarketWeb - Refactorizazioa/src/required/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de reacondicionamiento de ordenadores - BLACK MARKET</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="../css/index.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../../../../src/css/styleMenu.css" media="screen" />
+    <script src="https://kit.fontawesome.com/7f605dc8fe.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="">
+
+    <div class="myContainer">
+
+        <!-- hau da menua eta logoa, pagina guztietan berdina (hemen hasten da) -->
+
+        <nav id="menu">
+            <div id="botoiaSideBar">
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">MENU</button>
+            </div>
+
+            <div class="backGroundColorSideBar offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+                id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">BLACK MARKET</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                <a href="../../../../src/views/index/php/index.php">Guri buruz</a>
+                <a href="../../../../src/views/konponenteak/php/pagina_konponenteak.php">konponenteak</a>
+                <a href="../../../../src/views/berriak/php/berriak.php">Berriak</a>
+                <a href="../../../../src/views/Hornitzaileak/php/Hornitzaileak.php">Hornitzaileak</a>
+                </div>
+            </div>
+
+            <div class="search-form">
+                <input aria-label="Buscar" id="search-input" placeholder="Buscar" class="search-input" value="">
+                <button aria-label="Search" type="submit" class="search-button" id="search-button">Buscar</button>
+                
+            </div>
+
+        </nav>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                document.getElementById('search-button').addEventListener('click', function (e) {
+                    e.preventDefault();
+                    var searchTerm = document.getElementById('search-input').value;
+                    searchProducts(searchTerm);
+                });
+
+                function searchProducts(term) {
+                    var found = window.find(term, false, false, true, false, true, false);
+                    if (!found) {
+                        alert("No se encontraron coincidencias.");
+                    }
+                }
+            });
+        </script>
+        <!-- BILATZAILEAREN SCRIPTA -->
+        <!-- hau da menua eta logoa, pagina guztietan berdina (hemen bukatzen da) -->
